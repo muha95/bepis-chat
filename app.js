18f2062be0f38d4e464b809f04eac56bf0683f7c
Muhamed Delic <muhamed.delicc@live.com>
2016-09-10 18:19:18 +1000

    Added basic client functionality

--- app.js
+++ app.js
@@ -19,10 +19,17 @@ app.get("/", function(req, res) {
 // - There will be more crap to do.
 io.on("connection", function(socket) {
 	console.log("A client connected!");
+	socket.emit("bepis-message", "Server: Connection Established!");
 	socket.on("disconnect", function() {
 		console.log("A client disconnected!");
 	});
+	
+	socket.on("bepis-message", function(msg) {
+		io.emit("bepis-message", msg);
+	});
 });
+
+
 
 // Begin listening for incoming requests - default is 3000
 var port = process.env.PORT || 3000;
--- public/js/client-main.js
+++ public/js/client-main.js
@@ -3,3 +3,23 @@
 
 // Open a Socket.IO connection with the server.
 var socket = io();
+
+$("form").submit(function() {
+	if($("#msg-input").val()) {
+		socket.emit("bepis-message", $("#msg-input").val());
+		$("#msg-input").val("");
+		return false;
+	}
+});
+
+socket.on("connect", function() {
+	$("#connection-status").text("Status: Connected");
+});
+
+socket.on("disconnect", function() {
+	$("#connection-status").text("Status: Disconnected");
+});
+
+socket.on("bepis-message", function(msg) {
+	$("#container-messages").append($("<li>").text(msg));
+});
