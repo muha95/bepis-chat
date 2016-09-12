6a3a945eed158491491886af8535dab31951bbcb
Muhamed Delic <muhamed.delicc@live.com>
2016-09-12 23:01:59 +1000

    Updated Client-Side UI

--- public/js/client-main.js
+++ public/js/client-main.js
@@ -12,16 +12,33 @@ $("form").submit(function() {
 	}
 });
 
+applyMessageBoxHeight();
+
+function applyMessageBoxHeight() {
+	var titleHeight = $(".container-title").outerHeight();
+	var inputHeight = $(".container-input").outerHeight();
+	var messagesHeight = $(document).outerHeight() - titleHeight - inputHeight;
+	
+	$(".container-messages").css("height", messagesHeight);
+}
+
+$(window).on("resize", function() {
+	applyMessageBoxHeight();
+});
+
 socket.on("connect", function() {
-	$("#connection-status").text("Status: Connected");
-	$("#connection-status").addClass("connection-status-connected");
+	$("#connection-status").text("Connected");
+	$("#connection-status").removeClass("label-danger");
+	$("#connection-status").addClass("label-success");
 });
 
 socket.on("disconnect", function() {
-	$("#connection-status").text("Status: Disconnected");
-	$("#connection-status").removeClass("connection-status-connected");
+	$("#connection-status").text("Disconnected");
+	$("#connection-status").removeClass("label-success");
+	$("#connection-status").addClass("label-danger");
 });
 
 socket.on("bepis-message", function(msg) {
 	$("#message-list").append($("<li>").text(">	 " + msg));
 });
+
