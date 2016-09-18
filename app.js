4e78275f61e773f99e1e4446fd0ab7791347fe43
Muhamed Delic <muhamed.delicc@live.com>
2016-09-18 21:42:42 +1000

    added session test routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require("path");
 var express = require("express");
 var app = express();
 var session = require("express-session");
+var bodyParser = require("body-parser");
 
 var httpServer = require("http").Server(app);
 var io = require("socket.io")(httpServer);
@@ -12,8 +13,8 @@ app.use(session({
 	resave: false, // Don't force-save session data
 	saveUninitialized: false, // Don't save an uninit'd session
 	cookie: { 
-		httpOnly: false, 
-		secure: true,
+		//httpOnly: false, 
+		//secure: true,
 		maxAge: 3600 * 12 * 1000 /* 12 hour age-limit */ 
 	}
 	// name - option is used for the cookie's name
@@ -27,12 +28,59 @@ app.use(session({
 	// prone to memory leaks
 }));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTES - may move routes into it's own module as the
 // server API grows.
 app.get("/", function(req, res) {
 	res.sendFile("index.html");
+});
+
+app.get("/login", function(req, res) {
+	res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+var users = {
+	test: "test123"
+}
+
+app.post("/login", function(req, res) {
+	console.log(req.body);
+	var username = req.body.username;
+	var password = req.body.password;
+	if(username && password) {
+		console.log(users[username]);
+		if(users[username] && users[username] === password) {
+			console.log("password matches");
+			req.session.username = req.body.username;
+			res.redirect("/memberonly");
+		} else {
+			res.redirect("/login");
+		}
+	}
+});
+
+app.get("/memberonly", function(req, res) {
+	var username = req.session.username;
+	if(!req.session || !username) {
+		return res.redirect("/login");
+	}
+	res.end("Welcome to the member-only area "+username+"!!!");
+});
+
+app.get("/logout", function(req, res) {
+	if(!req.session || !req.session.username) {
+		return res.end("You're not even logged in! Click <a href=\"/login\">here</a> to login!");
+	}
+	req.session.destroy(function(err) {
+		if(err) {
+			console.log("error: "+err.message);
+		}
+	});
+	res.redirect("/login");
 });
 
 // Socket listening events
