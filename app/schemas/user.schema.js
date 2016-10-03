5fefb5a6728bc7f505b5b4fba1e6a85b2d571195
Muhamed Delic <muhamed.delicc@live.com>
2016-10-03 12:41:04 +1100

    Added validators for model fields

--- app/schemas/user.schema.js
+++ app/schemas/user.schema.js
@@ -7,19 +7,58 @@ var Schema = mongoose.Schema;
 module.exports = UserSchema;
 
 var UserSchema = new Schema({
-    username: { type: String, required: true, index: { unique: true } },
-    password: 
-    { 
-        type: String, 
-        required: true, 
-        validate: 
+    username:
+    {
+        type: String,
+        lowercase: true,
+        required: true,
+        index: { unique: true }
+        validate:
         {
-            validator: function(v) { return /^$/.test(v); }, 
-            message: "Password must contain at 6 characters, "+
-                    "include at least one upper-case and numerical character"
+            validator: function(input) {
+              return /^[a-zA-Z0-9]{4,}$/.test(input);
+            },
+            message: "Username must include at least 4 alphanumeric characters"
         }
     },
-    email: { type: String, required: true, index: { unique: true } },
+    password:
+    {
+        type: String,
+        required: true,
+        validate:
+        {
+            validator: function(input) {
+              // How this regex works...
+              // E.g., (?=.*?[A-Z]) this enforces an uppercase letter to be in the string
+              // --- ?= enforces a match but exclusion from capture (positive lookahead)
+              // --- .*? this allows the uppercase letters to be anywhere in the string
+              //     without it then it expects an uppercase letter first. Technically,
+              //     it goes through the entire string ("eats it up") and then backtracks through the
+              //     string until it finds a match.
+              // --- This is then repeated for all other required characters by using
+              //     subsequent lookaheads following the same pattern.
+              // --- On each new lookahead the cursor is reset to the beginning of the string.
+              return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{8,}$/.test(input);
+            },
+            message: "Password must contain at 8 characters, "+
+                     "include at least one lowercase, uppercase and numerical character"
+        }
+    },
+    email:
+    {
+        type: String,
+        lowercase: true,
+        required: true,
+        index: { unique: true },
+        validate:
+        {
+            validator: function(input) {
+              // http://emailregex.com/ - slightly modified the recommended regex
+              return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.([a-z]+)|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(input)
+            },
+            message: "Email is invalid"
+        }
+    },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     admin: { type: Boolean, default: false },
@@ -41,6 +80,7 @@ UserSchema.methods.comparePassword = function(inputToTest, callback) {
 
 UserSchema.pre("save", function(next) {
     var user = this;
+    // Only hash the password if it's been changed.
     if(!user.isModified("password")) {
         return next();
     }
